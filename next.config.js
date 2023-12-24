b6ee0b610664f14be6f55f81f9bd0ea1e3f0e65c
Muhammad Abubakar <111239185+Abe-27@users.noreply.github.com>
2023-12-24 12:28:12 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-module.exports = {
-  basePath: '',
-  assetPrefix: '',
-  images: {
-    loader: 'imgix',
-    path: '',
-    exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      // Add other paths as needed
-    };
-  },
-  },
-};
+// module.exports = {
+//   basePath: '',
+//   assetPrefix: '',
+//   images: {
+//     loader: 'imgix',
+//     path: '',
+//     exportPathMap: async function () {
+//     return {
+//       '/': { page: '/' },
+//       '/about': { page: '/about' },
+//       // Add other paths as needed
+//     };
+//   },
+//   },
+// };
 
